838256ac53d955b94344630bcfb3561c1511dcd7
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-04-10 18:40:05 -0300

    Added tests for quick_sort_m3 and hanoi_tower
    Co-authored-by: Sergio Cipriano <sergiosacj@hotmail.com.br>

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,7 @@
 from quick_sort import quick_sort
-from list_gen import random_generator, decreasing_generator, growing_generator, equal_generator
+from tower_of_hanoi import hanoi_tower
+from list_gen import random_generator, descending_generator, ascending_generator, equal_generator
+from quick_sort_med3 import quick_sort_m3
 
 import random
 import timeit
@@ -8,31 +10,76 @@ import timeit
 import sys
 sys.setrecursionlimit(100000)
 
-MAX = 0
+MAX = 5000
 COUNTER = 0
 
-RANDOM_LIST = random_generator(1000000)
-GROWING_LIST = growing_generator(1000000)
-DECREASING_LIST = decreasing_generator(1000000)
-EQUAL_LIST = equal_generator(1000000)
+RANDOM_LIST = random_generator(10000)
+GROWING_LIST = ascending_generator(10000)
+DECREASING_LIST = descending_generator(10000)
+EQUAL_LIST = equal_generator(10000)
+
+RANDOM_LIST2 = random_generator(10000)
+GROWING_LIST2 = ascending_generator(10000)
+DECREASING_LIST2 = descending_generator(10000)
+EQUAL_LIST2 = equal_generator(10000)
 
 def test ():
     """Test function"""
     if COUNTER == 1:
         quick_sort(RANDOM_LIST, 0, MAX)
-        print("LISTA ALEATORIA: ")
+        print("LISTA ALEATORIA: ", end = '')
     if COUNTER == 2:
         quick_sort(GROWING_LIST, 0, MAX)
-        print("LISTA CRESCENTE: ")
+        print("LISTA CRESCENTE: ", end = '')
     if COUNTER == 3:
         quick_sort(DECREASING_LIST, 0, MAX)
-        print("LISTA DECRESCENTE: ")
+        print("LISTA DECRESCENTE: ", end = '')
     if COUNTER == 4:
-        print("LISTA IDENTICA: ")
+        print("LISTA IDENTICA: ", end = '')
         quick_sort(EQUAL_LIST, 0, MAX)
 
+    if COUNTER == 5:
+        quick_sort_m3(RANDOM_LIST2, 0, MAX)
+        print("LISTA ALEATORIA: ", end = '')
+    if COUNTER == 6:
+        quick_sort_m3(GROWING_LIST2, 0, MAX)
+        print("LISTA CRESCENTE: ", end = '')
+    if COUNTER == 7:
+        quick_sort_m3(DECREASING_LIST2, 0, MAX)
+        print("LISTA DECRESCENTE: ", end = '')
+    if COUNTER == 8:
+        print("LISTA IDENTICA: ", end = '')
+        quick_sort_m3(EQUAL_LIST2, 0, MAX)
+
+    if COUNTER == 9:
+        print("Torre com 20 discos: ", end = '')
+        hanoi_tower(20, 1, 2, 3)
+    if COUNTER == 10:
+        print("Torre com 25 discos: ", end = '')
+        hanoi_tower(25, 1, 2, 3)
+    if COUNTER == 11:
+        print("Torre com 26 discos: ", end = '')
+        hanoi_tower(26, 1, 2, 3)
+    if COUNTER == 12:
+        print("Torre com 27 discos: ", end = '')
+        hanoi_tower(27, 1, 2, 3)
+        
+
 if __name__ == "__main__":
-    MAX = int(input()) - 1
-    for i in range(0, 4):
+
+    print("Quick_sort:")
+    for i in range(4):
+        COUNTER = COUNTER + 1
+        print(timeit.timeit("test()", setup="from __main__ import test",number = 1))
+
+    print("")
+    print("Quick_sort_m3:")
+    for i in range(4):
+        COUNTER = COUNTER + 1
+        print(timeit.timeit("test()", setup="from __main__ import test",number = 1))
+
+    print("")
+    print("Torre de Hanoi: ")
+    for i in range(4):
         COUNTER = COUNTER + 1
         print(timeit.timeit("test()", setup="from __main__ import test",number = 1))
