1c051357d19df1e125cde2d98f22436bc9ed4c84
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-04-09 22:24:44 -0300

    Testing quick_sort time
    Co-authored-by: Sergio Cipriano <sergiosacj@hotmail.com.br>

--- benchmark.py
+++ benchmark.py
@@ -1,8 +1,38 @@
+from quick_sort import quick_sort
+from list_gen import random_generator, decreasing_generator, growing_generator, equal_generator
+
+import random
+import timeit
+
+# Max recursion limit (default: 1000)
+import sys
+sys.setrecursionlimit(100000)
+
+MAX = 0
+COUNTER = 0
+
+RANDOM_LIST = random_generator(1000000)
+GROWING_LIST = growing_generator(1000000)
+DECREASING_LIST = decreasing_generator(1000000)
+EQUAL_LIST = equal_generator(1000000)
+
 def test ():
-    """Stupid test function"""
-    lst = []
-    for i in range(100):
-        lst.append(i)
+    """Test function"""
+    if COUNTER == 1:
+        quick_sort(RANDOM_LIST, 0, MAX)
+        print("LISTA ALEATORIA: ")
+    if COUNTER == 2:
+        quick_sort(GROWING_LIST, 0, MAX)
+        print("LISTA CRESCENTE: ")
+    if COUNTER == 3:
+        quick_sort(DECREASING_LIST, 0, MAX)
+        print("LISTA DECRESCENTE: ")
+    if COUNTER == 4:
+        print("LISTA IDENTICA: ")
+        quick_sort(EQUAL_LIST, 0, MAX)
 
 if __name__ == "__main__":
-    test()
+    MAX = int(input()) - 1
+    for i in range(0, 4):
+        COUNTER = COUNTER + 1
+        print(timeit.timeit("test()", setup="from __main__ import test",number = 1))
