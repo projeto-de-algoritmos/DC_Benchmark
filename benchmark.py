99ad4b9230e78d4bdf36e8bbc734ae52928ac654
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-04-11 19:04:53 -0300

    Refactoring tests in benchmark.py
    Co-authored-by: Sergio Cipriano <sergiosacj@hotmail.com.br>

--- benchmark.py
+++ benchmark.py
@@ -14,7 +14,7 @@ import random
 import timeit
 
 MAX = 5000
-COUNTER = 0
+COUNTER = -1
 
 RANDOM_LIST = random_generator(10000)
 GROWING_LIST = ascending_generator(10000)
@@ -26,63 +26,64 @@ GROWING_LIST2 = ascending_generator(10000)
 DECREASING_LIST2 = descending_generator(10000)
 EQUAL_LIST2 = equal_generator(10000)
 
+COMMAND_LIST = []
+PRINT_LIST = []
+
+def timer_test():
+    print(timeit.timeit("test()", setup="from __main__ import test",number = 1))
+
 def test ():
     """Test function"""
-    if COUNTER == 1:
-        quick_sort(RANDOM_LIST, 0, MAX)
-        print("LISTA ALEATORIA: ", end='')
-    if COUNTER == 2:
-        quick_sort(GROWING_LIST, 0, MAX)
-        print("LISTA CRESCENTE: ", end='')
-    if COUNTER == 3:
-        quick_sort(DECREASING_LIST, 0, MAX)
-        print("LISTA DECRESCENTE: ", end='')
-    if COUNTER == 4:
-        print("LISTA IDENTICA: ", end='')
-        quick_sort(EQUAL_LIST, 0, MAX)
-
-    if COUNTER == 5:
-        quick_sort_m3(RANDOM_LIST2, 0, MAX)
-        print("LISTA ALEATORIA: ", end='')
-    if COUNTER == 6:
-        quick_sort_m3(GROWING_LIST2, 0, MAX)
-        print("LISTA CRESCENTE: ", end='')
-    if COUNTER == 7:
-        quick_sort_m3(DECREASING_LIST2, 0, MAX)
-        print("LISTA DECRESCENTE: ", end='')
-    if COUNTER == 8:
-        print("LISTA IDENTICA: ", end='')
-        quick_sort_m3(EQUAL_LIST2, 0, MAX)
-
-    if COUNTER == 9:
-        print("Torre com 20 discos: ", end='')
-        hanoi_tower(20, 1, 2, 3)
-    if COUNTER == 10:
-        print("Torre com 25 discos: ", end='')
-        hanoi_tower(25, 1, 2, 3)
-    if COUNTER == 11:
-        print("Torre com 26 discos: ", end='')
-        hanoi_tower(26, 1, 2, 3)
-    if COUNTER == 12:
-        print("Torre com 27 discos: ", end='')
-        hanoi_tower(27, 1, 2, 3)
-        
+    print(PRINT_LIST[COUNTER], end = '')
+    eval(COMMAND_LIST[COUNTER])
 
 if __name__ == "__main__":
 
-    print("Quick Sort:")
-    for i in range(4):
-        COUNTER = COUNTER + 1
-        print(timeit.timeit("test()", setup="from __main__ import test",number = 1))
+    COMMAND_LIST.append('quick_sort(RANDOM_LIST, 0, MAX)')
+    PRINT_LIST.append("LISTA ALEATORIA: ")
 
-    print("")
-    print("Quick Sort com medianas de 3:")
-    for i in range(4):
-        COUNTER = COUNTER + 1
-        print(timeit.timeit("test()", setup="from __main__ import test",number = 1))
+    COMMAND_LIST.append('quick_sort(GROWING_LIST, 0, MAX)')
+    PRINT_LIST.append("LISTA CRESCENTE: ")
 
-    print("")
-    print("Torre de Hanoi: ")
-    for i in range(4):
+    COMMAND_LIST.append('quick_sort(DECREASING_LIST, 0, MAX)')
+    PRINT_LIST.append("LISTA DECRESCENTE: ")
+
+    COMMAND_LIST.append('quick_sort(EQUAL_LIST, 0, MAX)')
+    PRINT_LIST.append("LISTA IDENTICA: ")
+
+    COMMAND_LIST.append('quick_sort_m3(RANDOM_LIST2, 0, MAX)')
+    PRINT_LIST.append("LISTA ALEATORIA: ")
+
+    COMMAND_LIST.append('quick_sort_m3(GROWING_LIST2, 0, MAX)')
+    PRINT_LIST.append("LISTA CRESCENTE: ")
+
+    COMMAND_LIST.append('quick_sort_m3(DECREASING_LIST2, 0, MAX)')
+    PRINT_LIST.append("LISTA DECRESCENTE: ")
+
+    COMMAND_LIST.append('quick_sort_m3(EQUAL_LIST2, 0, MAX)')
+    PRINT_LIST.append("LISTA IDENTICA: ")
+
+    COMMAND_LIST.append('hanoi_tower(20, 1, 2, 3)')
+    PRINT_LIST.append("Torre com 20 discos: ")
+
+    COMMAND_LIST.append('hanoi_tower(25, 1, 2, 3)')
+    PRINT_LIST.append("Torre com 25 discos: ")
+
+    COMMAND_LIST.append('hanoi_tower(26, 1, 2, 3)')
+    PRINT_LIST.append("Torre com 26 discos: ")
+
+    COMMAND_LIST.append('hanoi_tower(27, 1, 2, 3)')
+    PRINT_LIST.append("Torre com 27 discos: ")
+
+    for i in range(len(COMMAND_LIST)):
+        if COUNTER == -1:
+            print("Quick Sort:")
+        if COUNTER == 3:
+            print("")
+            print("Quick Sort Mediana de 3:")
+        if COUNTER == 7:
+            print("")
+            print("Torre de Hanoi:")
+
         COUNTER = COUNTER + 1
-        print(timeit.timeit("test()", setup="from __main__ import test",number = 1))
+        timer_test()
